7caf1fd1b1ccf5e5da25bbcd3efc9079a76253ff
kalempster <mrpiter1337@gmail.com>
2022-11-27 02:01:20 +0100

    Fixed gap

--- frontend/src/components/Blog.ts
+++ frontend/src/components/Blog.ts
@@ -2,7 +2,7 @@ import { getLocale } from "../util/getLocale"
 
 export const BlogComponent = (title: string, shortContent: string, author: string, authorUrl: string, tags: string[], date: Date) => String.raw`
 <div
-    class="bg-gradient-to-tr py-10 from-rose-500 flex flex-col to-orange-500 rounded-lg shadow-lg duration-200 w-full">
+    class="bg-gradient-to-tr py-10 from-rose-500 gap-10 flex flex-col to-orange-500 rounded-lg shadow-lg duration-200 w-full">
     <div>
         <div class="px-10 text-2xl whitespace-pre-wrap font-medium text-white uppercase">${title}</div>
         <div class="px-10 whitespace-pre-wrap text-white">${shortContent}</div>
