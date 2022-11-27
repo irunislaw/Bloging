d5e08af854cb29bce820c8624da57de1febf182d
kalempster <mrpiter1337@gmail.com>
2022-11-27 04:57:43 +0100

    Navbar burger added + minor fixes

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,6 +1,5 @@
 import { BlogComponent } from "./components/Blog";
 import sanitize from "sanitize-html";
-import axios from "axios";
 
 const blogsDiv = document.querySelector("#blogs") as HTMLDivElement;
 
