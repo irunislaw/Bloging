dd22a7150f112888177f2b5b53e17b1de2918cc8
kalempster <mrpiter1337@gmail.com>
2022-11-28 00:20:10 +0100

    Added route list to origin

--- backend/api/VersionManager.ts
+++ backend/api/VersionManager.ts
@@ -6,6 +6,8 @@ export const router = Router();
 
 const versionDirectories = fs.readdirSync("./api", { withFileTypes: true }).filter(f => f.isDirectory());
 
+export const routes = [];
+
 for (const versionDirectory of versionDirectories) {
     const routesOfVersion = fs.readdirSync(`./api/${versionDirectory.name}`, { withFileTypes: true }).filter(f => f.isFile());
     for (const route of routesOfVersion) {
@@ -13,7 +15,7 @@ for (const versionDirectory of versionDirectories) {
         router.use(`/${versionDirectory.name}/${route.name.split(".")[0]}`, routeContents.router);
         for (const stack of routeContents.router.stack) {
             const routeProps = getRoutes(stack);
-            process.env.NODE_ENV === "development" ? console.log(`${Object.keys(routeProps.methods).join("|").toUpperCase()} ${process.env.API_ROOT}/${versionDirectory.name}/${route.name.split(".")[0]}${routeProps.path}`) : ""
+            routes.push(`${Object.keys(routeProps.methods).join("|").toUpperCase()} ${process.env.API_ROOT}/${versionDirectory.name}/${route.name.split(".")[0]}${routeProps.path}`);
         }
 
     }
--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config({ path: "../.env" });
 import * as express from "express";
 import mongoose from "mongoose";
-import { router } from "./api/VersionManager";
+import { router, routes } from "./api/VersionManager";
 import * as cors from "cors";
 const app = express();
 
@@ -13,6 +13,9 @@ const app = express();
 
     await mongoose.connect(process.env.MONGO_URI + "/" + process.env.MONGO_DB);
 
+    app.get("/", (req, res) => {
+        res.send(routes);
+    });
 
     app.listen(process.env.PORT, () => {
         console.log(`Listening on ${process.env.PORT}`);
