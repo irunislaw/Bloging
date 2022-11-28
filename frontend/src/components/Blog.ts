85d33a4855ff646fa4dfa7291adbfc9520399edd
Mikołaj Białecki <64149078+AViNIU@users.noreply.github.com>
2022-11-28 15:13:24 +0100

    Likes viewes add component

--- frontend/src/components/Blog.ts
+++ frontend/src/components/Blog.ts
@@ -3,7 +3,7 @@ import { getLocale } from "../util/getLocale"
 export const BlogComponent = (id: string, title: string, shortContent: string, author: string, authorUrl: string, tags: string[], date: Date, likes: number, views: number) => {
     
     return String.raw`
-<div class="blog bg-gradient-to-tr py-10 from-rose-500 gap-10 flex flex-col justify-between to-orange-500 rounded-lg shadow-lg duration-200 w-full"
+<div class="blog bg-gradient-to-tr py-10 from-rose-500 gap-10 flex flex-col justify-between to-orange-500 rounded-sm shadow-lg duration-200 w-full"
     onclick="addView('${id}')">
     <div>
         <div class="px-10 text-2xl whitespace-pre-wrap font-medium text-white uppercase">${title}</div>
@@ -16,10 +16,14 @@ export const BlogComponent = (id: string, title: string, shortContent: string, a
         </div>
         <div>
             <div class="font-normal float-none text-white pl-6 text-lg"> ${author}</div>
-            <div class="text-white pl-6 text-s text-light">${Intl.DateTimeFormat(getLocale(), {
+            <div class="text-white pl-6 text-s text-light flex">${Intl.DateTimeFormat(getLocale(), {
                 dateStyle:
                 "long"
-                }).format(date)} • ${tags.map((t) => " " + t)}</div>
+                }).format(date)} • ${tags.map((t) => " " + t)}
+                • <div class="capitalize">👀 ${views} • ❤ ${likes}</div>
+            </div>
+            
         </div>
+    
     </div>
 </div>`}
