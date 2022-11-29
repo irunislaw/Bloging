940322096af0cf2253aa0e00638413ecc4d1594e
kalempster <mrpiter1337@gmail.com>
2022-11-29 18:05:37 +0100

    Update sortByDate.ts

--- frontend/src/util/sortByDate.ts
+++ frontend/src/util/sortByDate.ts
@@ -3,5 +3,5 @@ import { Blog } from "../interfaces/Blog";
 export const sortByDate = (list: Blog[]) => list.sort((a, b) => {
     let date1 = new Date(a.createdAt);
     let date2 = new Date(b.createdAt);
-    return date1.getTime() - date2.getTime();
+    return date2.getTime() - date1.getTime();
 });
